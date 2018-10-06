03902408df6eb915200c3fd3a68bcaaa2e0e0a2b
JustTams <sarg.tier1@gmail.com>
2018-10-06 16:16:45 -0500

    final verison, with clean code (no moment cause it was a show)

--- assets/logic.js
+++ assets/logic.js
@@ -1,25 +1,27 @@
 // Firebase Config
 var config = {
-    apiKey: "AIzaSyDHyzxtQaqMgUgBbCKFtEMEhTNlZ-e5A4g",
-    authDomain: "train-schedule-1509c.firebaseapp.com",
-    databaseURL: "https://train-schedule-1509c.firebaseio.com",
-    projectId: "train-schedule-1509c",
-    storageBucket: "train-schedule-1509c.appspot.com",
-    messagingSenderId: "1040847280004"
-  };
-  firebase.initializeApp(config);
+  apiKey: "AIzaSyDHyzxtQaqMgUgBbCKFtEMEhTNlZ-e5A4g",
+  authDomain: "train-schedule-1509c.firebaseapp.com",
+  databaseURL: "https://train-schedule-1509c.firebaseio.com",
+  projectId: "train-schedule-1509c",
+  storageBucket: "train-schedule-1509c.appspot.com",
+  messagingSenderId: "1040847280004"
+};
+
+firebase.initializeApp(config);
 
   var database = firebase.database();
 
   // Button to add train information
   $("#submit-btn").on("click", function(event){
+    event.preventDefault();
     //variables for HTML Destinations
-    var trainName = $("data-name").val().trim();
-    var destination = $("data-destination").val().trim();
-    var start = $("data-start").val().trim();
-    var frequency = $("data-freq").val().trim();
-    debugger;
-  //local object to hold data before database  
+    var trainName = $("#data-name").val().trim();
+    var destination = $("#data-destination").val().trim();
+    var start = $("#data-start").val().trim();
+    var frequency = $("#data-freq").val().trim();
+
+    //local object to hold data before database  
   var newTrain = {
     name: trainName,
     destination: destination,
@@ -38,9 +40,36 @@ var config = {
 
   alert("New train has been added");
 
-  //clears the text boxes
-  trainName.val("");
-  destination.val("");
-  start.val("");
-  frequency.val("");
+  // clears the text boxes
+  $("#data-name").val("");
+  $("#data-destination").val("");
+  $("#data-start").val("");
+  $("#data-freq").val("");
+});
+
+// data from database to current train schedule (top card)
+database.ref().on("child_added", function (childSnapshot, prevChildKey) {
+  console.log(childSnapshot.val());
+
+  // store info from database into a new variable
+  var tName =  childSnapshot.val().name;
+  var dest = childSnapshot.val().destination;
+  var tStart = childSnapshot.val().start;
+  var hz = childSnapshot.val().frequency;
+
+  console.log(tName);
+  console.log(dest);
+  console.log(tStart);
+  console.log(hz);
+
+  // Makeas the train time start pretty
+  // var trainStartPretty = moment.unix(tStart).format(HH:MM);
+
+  // Add each train's data into the top card
+
+  $("#train-name").append("<div>" + tName + "</div>");
+  $("#destination").append("<div>" + dest + "</div>")
+  // $("#").append(tStart)
+  $("#frequency").append("<div>" + hz + "</div>")
+
 });
